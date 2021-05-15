9897f3cf7d38001923bb04aa1776408e60f121a3
rbaun <rainer.baun@berner-group.com>
2021-05-16 01:10:42 +0200

    improve playing start time

--- app/src/main/java/de/rayba/dmsinputservice/tvinput/Player.kt
+++ app/src/main/java/de/rayba/dmsinputservice/tvinput/Player.kt
@@ -3,21 +3,34 @@ package de.rayba.dmsinputservice.tvinput
 import android.content.Context
 import android.media.PlaybackParams
 import android.view.Surface
-import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.DefaultLoadControl
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.analytics.AnalyticsCollector
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
 import com.google.android.exoplayer2.extractor.ts.DefaultTsPayloadReaderFactory
+import com.google.android.exoplayer2.extractor.ts.TsExtractor
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Clock
+import com.google.android.exoplayer2.util.EventLogger
 import com.google.android.media.tv.companionlibrary.TvPlayer
 
+private const val DEFAULT_MAX_BUFFER_MS = 10000
+private const val DEFAULT_BUFFER_FOR_PLAYBACK_MS = 100
 
 class Player(val context: Context) : TvPlayer {
 
     private val trackSelector = DefaultTrackSelector(context)
-    private var player = initPlayer()
+    private val analyticsListener = EventLogger(trackSelector)
+    private val loadControl = DefaultLoadControl.Builder()
+        .setBufferDurationsMs(DEFAULT_MAX_BUFFER_MS,
+            DEFAULT_MAX_BUFFER_MS,
+            DEFAULT_BUFFER_FOR_PLAYBACK_MS,
+            DEFAULT_BUFFER_FOR_PLAYBACK_MS
+        ).build()
+    private val player = initPlayer()
 
     override fun seekTo(position: Long) {
         player.seekTo(position)
@@ -71,17 +84,25 @@ class Player(val context: Context) : TvPlayer {
     fun preparePlayer(url: String) {
         val extractorFactory = DefaultExtractorsFactory()
         extractorFactory.setTsExtractorFlags(DefaultTsPayloadReaderFactory.FLAG_DETECT_ACCESS_UNITS or DefaultTsPayloadReaderFactory.FLAG_ALLOW_NON_IDR_KEYFRAMES)
+        extractorFactory.setTsExtractorMode(TsExtractor.MODE_MULTI_PMT)
         val dataSourceFactory = DefaultDataSourceFactory(context, "dmsinputservice")
         val mediaItem = MediaItem.fromUri(url)
-        val mediaSource: ProgressiveMediaSource = ProgressiveMediaSource.Factory(dataSourceFactory, extractorFactory).createMediaSource(mediaItem)
+        val mediaSource: ProgressiveMediaSource =
+            ProgressiveMediaSource.Factory(dataSourceFactory, extractorFactory)
+                .createMediaSource(mediaItem)
         player.setMediaSource(mediaSource)
         player.prepare()
         player.play()
     }
 
-    private fun initPlayer(): SimpleExoPlayer {
+    fun initPlayer(): SimpleExoPlayer {
+        val analyticsCollector = AnalyticsCollector(Clock.DEFAULT)
+        analyticsCollector.addListener(analyticsListener)
         return SimpleExoPlayer.Builder(context)
             .setTrackSelector(trackSelector)
+            .setLoadControl(loadControl)
+            .setAnalyticsCollector(analyticsCollector)
             .build()
     }
+
 }
